09c577864ef64fc061c93f08b4e209f9fa8b9286
ThePalad1n <evangreen91@gmail.com>
2023-12-20 14:43:05 -0500

    build

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import TerminalComponent from '../src/Components/TerminalComponent';
-import ImageDisplayComponent from './Components/ImageDisplayComponent';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
     return (
         <div>
-            <Router>
-                <Routes>
-                    {/* Use the `element` prop to render components */}
-                    <Route path="/images" element={<ImageDisplayComponent />} />
-                    <Route path="/" element={<TerminalComponent />} />
-                </Routes>
-            </Router>
+            <TerminalComponent />
         </div>
     );
 };
