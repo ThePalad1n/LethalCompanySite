cbf0bcc4d75979fbc934011abe680355ce246a43
ThePalad1n <evangreen91@gmail.com>
2023-12-20 22:31:33 -0500

    UI fixes

--- src/Components/ImageDisplayComponent.jsx
+++ src/Components/ImageDisplayComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Accordion, Button } from 'react-bootstrap';
+import { Accordion } from 'react-bootstrap';
 import styled from 'styled-components';
 import step2image5 from '../assets/step2image5.JPG';
 
